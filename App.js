c9e7606646f391a60911bacf035298e1982800b1
Garima <pandeygarima704@gmail.com>
2024-10-01 19:44:16 -0400

    Navbar, Product listing

--- App.js
+++
@@ -1,7 +0,0 @@
-
-const heading = React.createElement("h1",{id:"heading"},[React.createElement("h1",{id:"heading"},"hello World"),
-React.createElement("h2",{id:"heading"},"Welcome")]);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
